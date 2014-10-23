b867ad5c6a33945627880d02d497f07e6328c421
Benny <benny.bauer@autodesk.com>
2014-10-23 11:30:48 +0300

    Converting to OpenStruct

--- global.rb
+++ global.rb
@@ -3,14 +3,22 @@ require 'singleton'
 class Settings
   include Singleton
 
+  attr_reader :data
+
   def initialize
-    @data = YAML::load(ERB.new(File.read("#{::Rails.root.to_s}/config/settings.yml")).result)[::Rails.env]
+    @data = structify(YAML::load(ERB.new(File.read("#{::Rails.root.to_s}/config/settings.yml")).result)[::Rails.env])
   end
 
-  def [](key)
-    key = key.to_s
-    ENV[key.upcase] || @data[key]
+  def structify(hash)
+    OpenStruct.new(
+      hash.reduce({}) do |r, i|
+        r[i[0]] = i[1].is_a?(Hash) ? structify(i[1]) : i[1]
+        r
+      end
+    )
   end
 end
-  
-GLOBAL ||= Settings.instance
+
+# TODO: implement ENV support
+
+GLOBAL ||= Settings.instance.data
